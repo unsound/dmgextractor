6410d84d07c4abbbd332bf4ae8ca739a7b99c2a4
Erik Larsson <catacombae@gmail.com>
2006-11-26 13:51:52 +0000

    Added my own XML parser to the tree under org.catacombae.xml.parser.

---
+++ src/org/catacombae/xml/parser/ExternalID.java
@@ -0,0 +1,19 @@
+package org.catacombae.xml.parser;
+
+public class ExternalID {
+    public static final int SYSTEM = 0;
+    public static final int PUBLIC = 1;
+    public int type;
+    public String pubidLiteral;
+    public String systemLiteral;
+
+    public ExternalID(String pubidLiteral, String systemLiteral) {
+	this.pubidLiteral = pubidLiteral;
+	this.systemLiteral = systemLiteral;
+	this.type = PUBLIC;
+    }
+    public ExternalID(String systemLiteral) {
+	this.systemLiteral = systemLiteral;
+	this.type = SYSTEM;
+    }
+}
