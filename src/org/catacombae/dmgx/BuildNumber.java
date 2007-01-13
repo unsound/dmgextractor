c75a5505bd132cd09f73969f986007d331cb37bf
Erik Larsson <catacombae@gmail.com>
2007-01-13 15:35:32 +0000

    Version 0.51pre1.

--- src/org/catacombae/dmgx/BuildNumber.java
+++ src/org/catacombae/dmgx/BuildNumber.java
@@ -22,7 +22,7 @@ package org.catacombae.dmgx;
 
 public class BuildNumber {
     //[BuildEnumerator:Opening] WARNING: The following lines are managed by an external program. Do NOT change.
-    public static final long BUILD_NUMBER = 225L;
+    public static final long BUILD_NUMBER = 226L;
     //[BuildEnumerator:Closing] The lines managed by an external program end here.
 }
 
