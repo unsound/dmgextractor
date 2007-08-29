070a8cc570c638fcd26d0826899b48a4d8e49e68
Erik Larsson <catacombae@gmail.com>
2007-08-29 05:25:09 +0000

    Updated sh script files to deal with new tree structure. Not verified.

--- extractplist.sh
+++ extractplist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp lib/dmgextractor.jar org.catacombae.dmgx.ExtractPlist $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -cp dist/lib/dmgextractor.jar:dist/lib/apache-ant-1.7.0-bzip2.jar:dist/lib/filedrop.jar:dist/lib/iharder-base64.jar:dist/lib/swing-layout-1.0.1-stripped.jar org.catacombae.dmgx.ExtractPlist $1 $2 $3 $4 $5 $6 $7 $8 $9
