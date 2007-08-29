bde6aca449d863daebe6a212a207671bf245a65b
Erik Larsson <catacombae@gmail.com>
2007-08-29 05:15:33 +0000

    Reorganization and cleanup.
    - Removed a few debug printlns in DMGBlockInputStream.
    - Modified build scripts to accommodate the new tree structure.
    - Further reorganization.

--- dist/dmgx.bat
+++ dist/dmgx.bat
@@ -1,4 +1,5 @@
 @echo off
-pushd %~dp0
-java -cp lib\dmgextractor.jar org.catacombae.dmgx.DMGExtractor -startupcommand dmgx %1 %2 %3 %4 %5 %6 %7 %8 %9
-popd
+setlocal
+set DMGX_CP="%~dp0lib\dmgextractor.jar";"%~dp0lib\apache-ant-1.7.0-bzip2.jar";"%~dp0lib\iharder-base64.jar"
+java -cp %DMGX_CP% org.catacombae.dmgx.DMGExtractor -startupcommand dmgx %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
