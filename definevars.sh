2878913a7760e6658999d57c27d53e12eb092d2b
Erik Larsson <catacombae@gmail.com>
2008-10-10 14:20:52 +0000

    Build fixes for the new project structure.

--- definevars.sh
+++ definevars.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-LIBDIR=targets/application/lib
+export LIBDIR="targets/application/lib"
 
-DMGX_CLASSPATH=$LIBDIR/dmgextractor.jar:$LIBDIR/catacombae_io.jar:$LIBDIR/apache-ant-1.7.0-bzip2.jar:$LIBDIR/filedrop.jar:$LIBDIR/iharder-base64.jar:$LIBDIR/swing-layout-1.0.3.jar
+export DMGX_CLASSPATH="$LIBDIR/dmgextractor.jar:$LIBDIR/catacombae_io.jar:$LIBDIR/apache-ant-1.7.0-bzip2.jar:$LIBDIR/filedrop.jar:$LIBDIR/iharder-base64.jar:$LIBDIR/swing-layout-1.0.3.jar"
+echo "$DMGX_CLASSPATH"
