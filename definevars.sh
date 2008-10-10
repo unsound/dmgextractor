9023cb0237dde8f995b33415fab126606f757f0d
Erik Larsson <catacombae@gmail.com>
2008-10-10 14:05:17 +0000

    Re-add .sh scripts as binary.

---
+++ definevars.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+LIBDIR=targets/application/lib
+
+DMGX_CLASSPATH=$LIBDIR/dmgextractor.jar:$LIBDIR/catacombae_io.jar:$LIBDIR/apache-ant-1.7.0-bzip2.jar:$LIBDIR/filedrop.jar:$LIBDIR/iharder-base64.jar:$LIBDIR/swing-layout-1.0.3.jar
---
+++ dmgx.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./definevars.sh
+
+java -cp $DMGX_CLASSPATH org.catacombae.dmgextractor.DMGExtractor -startupcommand "$0" "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
