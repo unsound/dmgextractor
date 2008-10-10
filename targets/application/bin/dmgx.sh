3745c229280b2101983ef9ab071e1a6b19ce451e
Erik Larsson <catacombae@gmail.com>
2008-10-10 13:59:16 +0000

    Removing 'dmgx.sh' in order to change its type to binary.

--- targets/application/bin/dmgx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DMGX_CP=../lib/dmgextractor.jar:../lib/apache-ant-1.7.0-bzip2.jar:../lib/iharder-base64.jar:../lib/catacombae_io.jar
-java -cp $DMGX_CP org.catacombae.dmgextractor.DMGExtractor -startupcommand "$0" "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
