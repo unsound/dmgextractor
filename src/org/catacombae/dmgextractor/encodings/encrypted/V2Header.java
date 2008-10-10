5b9974a610926e6b3cba657f3f722b85014b15d1
Erik Larsson <catacombae@gmail.com>
2008-10-10 11:00:19 +0000

    Added ReadableCEncryptedEncodingStream and updated license texts.

--- src/org/catacombae/dmgextractor/encodings/encrypted/V2Header.java
+++ src/org/catacombae/dmgextractor/encodings/encrypted/V2Header.java
@@ -1,3 +1,42 @@
+/*-
+ * Copyright (C) 2008 Erik Larsson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This code was written from studying vfdecrypt, Copyright (c) 2006
+ *   Ralf-Philipp Weinmann <ralf@coderpunks.org>
+ *   Jacob Appelbaum <jacob@appelbaum.net>
+ *   Christian Fromme <kaner@strace.org>
+ *
+ * [I'm not sure if their copyright and license terms need to be applied,
+ *  but in case they do, the original license terms are reprinted below
+ *  as required by the license.]
+ *
+ * The vfdecrypt license says:
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ */
+
 package org.catacombae.dmgextractor.encodings.encrypted;
 
 import java.io.PrintStream;
