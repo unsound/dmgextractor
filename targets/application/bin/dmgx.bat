e57779ba1768b9495b1f3d3dc84b7a5059fd1d91
Erik Larsson <catacombae@gmail.com>
2008-11-10 06:05:57 +0000

    Remove dmgx.bat in order to change its type to binary.

--- targets/application/bin/dmgx.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set DMGX_CP="%~dp0..\lib\dmgextractor.jar"
-java -cp %DMGX_CP% org.catacombae.dmgextractor.DMGExtractor -startupcommand dmgx %1 %2 %3 %4 %5 %6 %7 %8 %9
-endlocal
