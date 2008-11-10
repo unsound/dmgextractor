0ec039d817b47b1aa3c7b361a55eac636ad4d8a8
Erik Larsson <catacombae@gmail.com>
2008-11-10 06:44:41 +0000

    Remove recently added scripts in order to change their type to binary.
    Stupid me...

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-BASE=dist
-DMGX_CP=$BASE/lib/dmgextractor.jar:$BASE/lib/apache-ant-1.7.0-bzip2.jar:$BASE/lib/iharder-base64.jar
-java -cp $DMGX_CP org.catacombae.$1 $2 $3 $4 $5 $6 $7 $8 $9
