a47eaf4adc1e20ef816e185a821405425024f5e3
Erik Larsson <catacombae@gmail.com>
2008-11-16 10:15:32 +0000

    Updated DMGExtractor to use csframework instead of catacombae_io.

--- definevars.sh
+++ definevars.sh
@@ -2,5 +2,5 @@
 
 export LIBDIR="targets/application/lib"
 
-export DMGX_CLASSPATH="$LIBDIR/dmgextractor.jar:$LIBDIR/catacombae_io.jar:$LIBDIR/apache-ant-1.7.0-bzip2.jar:$LIBDIR/filedrop.jar:$LIBDIR/iharder-base64.jar:$LIBDIR/swing-layout-1.0.3.jar"
+export DMGX_CLASSPATH="$LIBDIR/dmgextractor.jar"
 echo "$DMGX_CLASSPATH"
