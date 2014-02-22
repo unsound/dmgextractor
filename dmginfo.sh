d1131bc0aba9c13cbc2e031eefcac06cdaddc77d
Erik Larsson <catacombae@gmail.com>
2014-02-22 09:23:06 +0100

    Fix outdated class references in script files.

--- dmginfo.sh
+++ dmginfo.sh
@@ -2,4 +2,4 @@
 
 DMGX_CLASSPATH=`./definevars.sh`
 
-java -cp "$DMGX_CLASSPATH" org.catacombae.dmgx.DMGInfoWindow $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -cp "$DMGX_CLASSPATH" org.catacombae.dmgextractor.utils.DMGInfoWindow $1 $2 $3 $4 $5 $6 $7 $8 $9
